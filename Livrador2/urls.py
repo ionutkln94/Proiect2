3aff9f489c6a4b61f3095f78714dbf6f689f6a68
ionutkln94 <kln.ionut94@gmail.com>
2019-06-03 17:49:28 +0300

    v3

--- Livrador2/urls.py
+++ Livrador2/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from django.conf.urls import url
+from Web import views
+from django.contrib.auth import views as auth_views
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
+
+    re_path(r'^supermarket/sign-in/', auth_views.LoginView.as_view(template_name='supermarket/sign-in.html'), name='supermarket_sign_in'),
+    re_path(r'^supermarket/sign-out/', auth_views.LogoutView.as_view(template_name='/'), name='supermarket_sign_out'),
+
+    re_path(r'^supermarket/sign-up/', views.supermarket_sign_up, name='supermarket_sign_up'),
+
+    re_path(r'^supermarket/$', views.supermarket_home, name = 'supermarket_home')
 ]
